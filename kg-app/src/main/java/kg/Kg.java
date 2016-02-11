8e13d66286db1b3642783d4f1f259c667a9877b6
Henryk Konsek <hekonsek@gmail.com>
2016-02-11 14:10:38 +0100

    Added local store for camera images.

--- kg-app/src/main/java/kg/Kg.java
+++ kg-app/src/main/java/kg/Kg.java
@@ -6,6 +6,8 @@ import org.apache.camel.builder.RouteBuilder;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 
+import java.io.File;
+
 import static io.rhiot.cloudplatform.connector.Header.arguments;
 
 public class Kg extends CloudPlatform {
@@ -13,13 +15,21 @@ public class Kg extends CloudPlatform {
     @Autowired
     IoTConnector connector;
 
+    File localStore = new File("/var/rhiot/gateway/camera");
+
     @Bean
     RouteBuilder webcam() {
         return new RouteBuilder() {
             @Override
             public void configure() throws Exception {
+                localStore.mkdir();
+
                 from("webcam:label?motion=true&format=JPG").
-                        to("log:CameraNotification").
+                        to("log:CameraMotionNotification").
+                        to("file://" + localStore.getAbsolutePath());
+
+                        from("file://" + localStore.getAbsolutePath() + "?sortBy=file:modified").
+                        onException(Exception.class).maximumRedeliveries(100000).useExponentialBackOff().end().
                         process(exc -> connector.toBus("camera.process", exc.getIn().getBody(byte[].class),
                                 arguments("cameraHall0002", "eu")));
             }
